2c147718bde41ac19df217f024d480b2f426d6eb
503602501 <503602501@qq.com>
2020-11-15 12:58:47 +0800

    auto

--- framework/init.py
+++ framework/init.py
@@ -1,18 +1,12 @@
 
-
-#一端认证的代码块#
-
-#动态生成字段种包含账号和密码的加密信息
+import  hashlib
 
 key =globals()["key"]
-
-message='Hello world!xxxxx'
-print("key:"+key)
-
-# 复杂运算 key 和# message ,输出data
-data = message+"iiiiii"
-
-file = open("./local_key",'w')
-file.write(data+key)
+account =globals()["account"]
+m = hashlib.md5()
+m.update(str.encode("utf8"))
+file = open("./"+m.hexdigest(),'w',encoding="utf-8")
+file.write(key)
 file.close()
+
 
