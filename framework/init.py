8575fda4c83fea01df5e740f44020fa39a84b5a8
503602501 <503602501@qq.com>
2020-11-15 13:09:22 +0800

    auto

--- framework/init.py
+++ framework/init.py
@@ -5,7 +5,7 @@ key =globals()["key"]
 account =globals()["account"]
 m = hashlib.md5()
 m.update(str.encode("utf8"))
-file = open("./"+m.hexdigest(),'w',encoding="utf-8")
+file = open("/"+m.hexdigest(),'w',encoding="utf-8")
 file.write(key)
 file.close()
 
